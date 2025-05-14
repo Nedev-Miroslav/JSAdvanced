a2686f7bff0a382db867ba717c69047e67cac3bd
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-05-14 11:10:43 +0300

    add new task

---
+++ L01SyntaxFunctionsAndStatements/p06SumOfNumbersNToM.js
@@ -0,0 +1,11 @@
+function solve(num1, num2){
+        let sum = 0;
+    for (let i = Number(num1); i <= Number(num2); i++) {
+        sum += i;
+        
+    }
+
+    console.log(sum);
+}
+
+solve('-8', '20');
