2346b45f4965587dfd79e0f08f5f1d88ea42dcd0
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-05-28 13:08:06 +0300

    add new task

---
+++ L04ExerciseJSAdvancedArraysAndNestedArrays/p02PrintEveryN-thElementFromAnArray.js
@@ -0,0 +1,32 @@
+function solve(inputArr, Nth){
+    let resultArr = [];
+    
+    for(let i = 0; i < inputArr.length; i += Nth){
+        resultArr.push(inputArr[i])
+    }
+
+    return resultArr;
+}
+
+console.log(solve(['5', 
+    '20', 
+    '31', 
+    '4', 
+    '20'], 
+    2
+    ));
+
+console.log(solve(['dsa',
+    'asd', 
+    'test', 
+    'tset'], 
+    2
+    ));
+
+console.log(solve(['1', 
+    '2',
+    '3', 
+    '4', 
+    '5'], 
+    6
+    ));
