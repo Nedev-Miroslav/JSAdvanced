5fdbda48dacac9e8007c022e671e19a48f9cb1d4
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-05-28 13:07:16 +0300

    add new task

---
+++ L04ExerciseJSAdvancedArraysAndNestedArrays/p01PrintAnArrayWithAGivenDelimiter.js
@@ -0,0 +1,12 @@
+function solve(inputArr, delimiter){
+    let result = inputArr.join(delimiter);
+    console.log(result);
+}
+
+solve(['One', 
+    'Two', 
+    'Three', 
+    'Four', 
+    'Five'], 
+    '-'
+    );
