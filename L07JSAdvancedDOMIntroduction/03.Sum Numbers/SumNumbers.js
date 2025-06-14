1696728371aacebd1f450a2b2fe4a9402b10eb79
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-06-14 11:19:34 +0300

    add new task

---
+++ L07JSAdvancedDOMIntroduction/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,12 @@
+function calc() {
+    const num1 = document.getElementById("num1");
+    const num2 = document.getElementById("num2");
+    const sum = document.getElementById("sum");
+
+
+    const firstNum = Number(num1.value);
+    const secondNum = Number(num2.value);
+    
+    sum.value = firstNum + secondNum;
+    
+}
