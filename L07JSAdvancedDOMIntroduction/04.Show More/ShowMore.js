5d9df74b2338eb57fcc1eb2c63ca9b90fa89f768
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-06-15 12:02:31 +0300

    add new task

---
+++ L07JSAdvancedDOMIntroduction/04.Show More/ShowMore.js
@@ -0,0 +1,10 @@
+function showText() {
+    let hidenText = document.getElementById("text");
+    
+    hidenText.style.display = 'inline';
+    
+    let button = document.getElementById("more");
+
+    button.style.display = "none";
+
+}
